d7ec1253874105e85cad6d5b6fa1bb08a8102b0e
Gabriel Martins Spínola <martinsspn@gmail.com>
2019-10-03 19:27:35 -0300

    plementação

--- levajeito/guarda.py
+++ levajeito/guarda.py
@@ -5,9 +5,9 @@ import hashlib
 import getopt
 import hmac
 
-def filesList(pasta): #Função para adicionar todos os arquivos da pasta em uma lista
+def filesList(diretorio): #Função para adicionar todos os arquivos da pasta em uma lista
     files = []
-    for r, d, f in os.walk(pasta):
+    for r, d, f in os.walk(diretorio):
         for file in f:
             if str(os.path.join(r, file)) == str(diretorio+'/.guarda.txt'):
                 continue
@@ -20,48 +20,14 @@ def filesList(pasta): #Função para adicionar todos os arquivos da pasta em uma
             files.append(os.path.join(r, file))
     return files #Retorna a lista com as funções
 
-argv = sys.argv[1:]
-
-saida = ''
-try:
-    opts, args = getopt.getopt(argv, "i:t:x:o:", ["hash", "hmac="]) #usando o get opt para pegar as informações do terminal
-except getopt.GetoptError as err:
-    print(err)
-    sys.exit()
-
-for opt, arg in opts:
-    if opt == '-i':
-        opcao = 0
-        pasta = arg
-    elif opt == '-t':
-        opcao = 1
-        pasta = arg
-    elif opt == '-x':
-        opcao = 2
-        pasta = arg
-    elif opt == '-o':
-        saida = arg
-    elif opt == '--hash':
-        metodo = 0
-    elif opt == '--hmac':
-        metodo = 1
-        senha = arg
-
-
-if saida != '': #caso a opção -o for executada abrirá um arquivo para imprimir a saida do programa
-    arq_saida = open(saida, 'a')
-if opcao == 0: #opção = 0 -> executando a opção -i
+def iniciar(pasta, metodo, senha):
     verificacao = open('.listaGuarda.txt', 'a+') #verifica numa lista que estará na mesma pasta que o programa guarda se a pasta já está sendo monitorada
     verificacao.seek(0)
     os.chdir(pasta) #pegando o caminho completo da pasta
     diretorio = os.getcwd()
     for linha in verificacao: #verificacao se já está sendo monitorado
         if diretorio+"\n" == linha:
-            if saida == '':
-                print ("Pasta já está sendo monitorada")
-            else:
-                arq_saida.write("Pasta já está sendo monitorada\n")
-            sys.exit()
+            return -1
     guarda = open(diretorio+'/.guarda.txt', 'w') #cria um arquivo txt numa pasta dentro da pasta monitorada onde esse txt guarda os hash's dos arquivos da pasta monitorada
     if metodo == 0: #se estiver sendo executado a opção --hash imprimira no arquivo oculto o valor correspondente ao metodo hash (0)
         guarda.write(str(metodo)+"\n")
@@ -84,12 +50,9 @@ if opcao == 0: #opção = 0 -> executando a opção -i
         arquivo = files[i]
         guarda.write("/" + arquivo[1:] + " > " + hashes[i].hexdigest() + "\n") #escreve no arquivo dentro da pasta monitorada os hash's dos arquivos
     guarda.close()
-    if saida == '': #se -o não for executado imprime na tela
-        print ("A pasta está sendo monitorada!!!")
-    else: #se -o está sendo executado imprime no arquivo de saida
-        arq_saida.write("A pasta está sendo monitorada!!!\n")
+    return 0
 
-elif opcao == 1: #opção = 1 -> executando a opção -t
+def tracking(pasta, senha, saida):
     verificacao = open('.listaGuarda.txt', 'r') #abre arquivo de verificação das pastas que estão sendo monitoradas
     verificacao.seek(0)
     os.chdir(pasta) #pegando caminho completo da pasta
@@ -143,30 +106,30 @@ elif opcao == 1: #opção = 1 -> executando a opção -t
                 else:
                     arq_saida.write("O arquivo " + i + " foi adicionado!!!\n") #imprime no arquivo de saida
 
-    else: #se o tracking for falso então a pasta não está sendo monitorada
-        if saida == '': 
-            print("Diretorio não está sendo monitorada pelo guarda!!!")
-        else:
-            arq_saida.write("Diretorio não está sendo monitorada pelo guarda!!!\n")
-        sys.exit()
-
-elif opcao == 2: #opcao = 2 -> executando a opcao -x 
+def desabilitar(pasta):
     if pasta == "all": #se o parametro do -x for a palavra 'all' então o guarda desabilitara a guarda para todas as pastas
         verificacao = open('.listaGuarda.txt', 'r') #abre a lista que contem as pastas selecionadas
         for linha in verificacao: #apagando os arquivos ocultos de cada pasta que está sendo monitorada
             os.remove(linha[:len(linha)-1]+'/.guarda.txt') 
         verificacao.close()
         os.remove('.listaGuarda.txt')#por fim apaga o arquivo de lista de pastas pois a lista estará vazia já que nenhuma pasta está sendo monitorada
-        if saida == '':
-            print ("Desabilitando monitoramento para todas as pastas monitoradas!!!")
-        else:
-            arq_saida.write("Monitoramento desligado para todas as pastas!!!\n")
+        return 0
     else: #desabilita o tracking apenas para a pasta que está sendo passada por parametro do -x
         os.chdir(pasta) #pegando caminho completo da pasta
-        apagar = os.getcwd()+'/.guarda.txt'
-        os.remove(apagar) #removendo o arquivo oculto da pasta informada
+        apagar = os.getcwd()
         verificacao = open('.listaGuarda.txt', 'r')
+        existe = False
+        for linha in verificacao:
+            if apagar+"\n" == linha:
+                existe = True
+                break
+        apagar += '/.guarda.txt'
+        if existe == True:
+            os.remove(apagar) #removendo o arquivo oculto da pasta informada
+        else:
+            return 2
         auxiliar = open('.auxiliar.txt', 'w')
+        verificacao.seek(0)
         for linha in verificacao: #cria um arquivo auxiliar para apagar a informação de tracking na lista de tracking do programa
             if linha[:len(linha)-1]+'/.guarda.txt' == apagar:
                 continue
@@ -175,10 +138,96 @@ elif opcao == 2: #opcao = 2 -> executando a opcao -x
         auxiliar.close()
         os.remove('.listaGuarda.txt') #apaga a lista de monitoramento que tem a pasta que foi desabiltada
         os.rename('.auxiliar.txt', '.listaGuarda.txt') #renomeia o arquivo auxiliar para o nome original da lista de monitoramento, agora sem a pasta desabilitada
+        return 1
+            
+argv = sys.argv[1:]
+
+saida = ''
+senha = ''
+pasta = ''
+
+try:
+    opts, args = getopt.getopt(argv, "i:t:x:o:", ["hash", "hmac=", "push="]) #usando o get opt para pegar as informações do terminal
+except getopt.GetoptError as err:
+    print(err)
+    sys.exit()
+
+for opt, arg in opts:
+    if opt == '-i':
+        opcao = 0
+        pasta = arg
+    elif opt == '-t':
+        opcao = 1
+        pasta = arg
+    elif opt == '-x':
+        opcao = 2
+        pasta = arg
+    elif opt == '-o':
+        saida = arg
+    elif opt == '--hash':
+        metodo = 0
+    elif opt == '--hmac':
+        metodo = 1
+        senha = arg
+    elif opt == '--push':
+        opcao = 3
+        pasta = arg
+
+if saida != '': #caso a opção -o for executada abrirá um arquivo para imprimir a saida do programa
+    arq_saida = open(saida, 'a')
+if opcao == 0: #opção = 0 -> executando a opção -i
+    if iniciar(pasta, metodo, senha) == 0:
+        if saida == '': #se -o não for executado imprime na tela
+            print ("A pasta está sendo monitorada!!!")
+        else: #se -o está sendo executado imprime no arquivo de saida
+            arq_saida.write("A pasta está sendo monitorada!!!\n")
+    else: 
+        if saida == '':
+            print ("Pasta já está sendo monitorada")
+        else:
+            arq_saida.write("Pasta já está sendo monitorada\n")
+    
+elif opcao == 1: #opção = 1 -> executando a opção -t
+    tracking(pasta, senha, saida)
+    
+elif opcao == 2: #opcao = 2 -> executando a opcao -x 
+    x = desabilitar(pasta)
+    if x == 0:
+        if saida == '':
+            print ("Desabilitando monitoramento para todas as pastas monitoradas!!!")
+        else:
+            arq_saida.write("Monitoramento desligado para todas as pastas!!!\n")
+    elif x == 1:
         if saida == '':
             print("Monitoramento para a pasta foi desabilitado!!!")
         else:
             arq_saida.write("Monitoramento para a pasta foi desabilitado!!!\n")
+    else:
+        if saida == '':
+            print("A pasta selecionada não está sendo monitorada no momento!!!")
+        else:
+            arq_saida.write("A pasta selecionada não está sendo monitorada no momento!!!\n")
+
+elif opcao == 3:
+    os.chdir(pasta) #pegando caminho completo da pasta
+    diretorio = os.getcwd()
+    getinfo = open(diretorio+'/.guarda.txt', 'r')
+    auxiliar = getinfo.readline()
+    getinfo.seek(0)
+    getinfo.close()
+    metodo = int(auxiliar[0])
+    senha = auxiliar[1:len(auxiliar)-1]
+    if desabilitar(pasta) == 1:
+        if iniciar(pasta, metodo, senha) == 0:
+            if saida == '':
+                print("Monitoramento atualizado com sucesso!!!")
+            else:
+                arq_saida.write("Monitoramento atualizado com sucesso!!!\n")
+    else:
+        if saida == '':
+            print("Falha na atualização!!!")
+        else:
+            arq_saida.write("Falha na atualização!!!\n")
 
 if saida != '':
     arq_saida.close() #fechando o arquivo de saida
